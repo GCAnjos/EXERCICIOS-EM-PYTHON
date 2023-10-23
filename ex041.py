77a02766442382a4bb34a65905417ec1639cb084
GCAnjos <146896104+GCAnjos@users.noreply.github.com>
2023-10-23 20:52:52 -0300

    Bem vindo

---
+++ ex041.py
@@ -0,0 +1,15 @@
+from datetime import date
+atual = date.today().year
+nascimento = int(input('Qual o seu ano de nascimento? '))
+idade = atual - nascimento
+print('O atleta tem {} anos.'.format(idade))
+if idade <= 9:
+    print('Classificação MIRIM.')
+elif idade >= 10 and idade <= 14:
+    print('Classificação INFANTIL:')
+elif idade >= 15 and idade <= 19:
+    print('Classificação JUNIOR.')
+elif idade >= 20 and idade <= 25:
+    print('Classificação SENIOR.')
+else:
+    print('Classificação MASTER.')
