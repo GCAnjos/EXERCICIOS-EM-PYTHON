616ff89e7e39e45682d68c311399e29d614416d5
Gabriel Cardoso dos Anjos <146896104+GCAnjos@users.noreply.github.com>
2023-12-12 22:21:27 -0300

    Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar ou não. No final, mostre: A) qual é o total gasto na compra. B) quantos produtos custam mais de R$1000. C) qual é o nome do produto mais barato.

--- ex070.py
+++ ex070.py
@@ -22,3 +22,4 @@ print('{:-^40}'.format('Fim do programa.'))
 print(f'O total da compra foi R$ {total:.2f}')
 print(f'Temos {totmil} produtos custando mais de R$ 1.000,00')
 print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
+
